cabf6f7459a4211a6c6bc3574757b151740908fb
GYXGY <49230088+GYXGY@users.noreply.github.com>
2019-04-08 14:47:44 +0800

    Update MergeFiles.py

--- MergeFiles.py
+++ MergeFiles.py
@@ -1,17 +1,20 @@
 import pandas as pd  
 import os  
-
+from time import time
 SaveFile_Name = r"E:\dataset\MachineLearningCVE\CICIDS2017.csv"
 LoadFile_Name = "E:\dataset\MachineLearningCVE"
 
 #load csvfiles
 file_list = os.listdir(LoadFile_Name)  
 
-#merge the first csvfile
-data = pd.read_csv(os.path.join(LoadFile_Name,file_list[0]),low_memory=False) 
-data.to_csv(SaveFile_Name,index=False,mode='a+')  
+t0 = time()
+#merge first csvfile
+df = pd.read_csv(os.path.join(LoadFile_Name,file_list[0]),low_memory=False) 
+df.to_csv(SaveFile_Name,index=False,mode='a+')  
   
 #merge the other csvfiles
 for i in range(1,len(file_list)):  
-    data = pd.read_csv(os.path.join(LoadFile_Name,file_list[i]),low_memory=False)
-    data.to_csv(SaveFile_Name,index=False,header=False,mode='a+')
+    df = pd.read_csv(os.path.join(LoadFile_Name,file_list[i]),low_memory=False)
+    df.to_csv(SaveFile_Name,index=False,header=False,mode='a+')
+tt = time()-t0
+print ("Merge files in {} seconds".format(round(tt,3)))
